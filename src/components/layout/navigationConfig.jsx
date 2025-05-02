f0d915eb5408aac663e8d33ba70377c5157a61c3
bguerschom <bigirimanaguerschom@gmail.com>
2025-05-02 17:22:10 +0200

    After changing the user role access

--- src/components/layout/navigationConfig.jsx
+++ src/components/layout/navigationConfig.jsx
@@ -246,57 +246,11 @@ export const roleBasedNavigation = {
       path: '/userdashboard', 
       icon: Home 
     },
-    { 
-      name: 'Guard Shift Report', 
-      path: '/guard-shift', 
-      icon: ClipboardList
-    },
-    {
-      name: 'Background Checks',
-      icon: Shield,
-      children: [
-        {
-          name: 'New Request',
-          path: '/new-request',
-          icon: FileCheck
-        },
-        {
-          name: 'Intership Overview',
-          path: '/internshipoverview',
-          icon: Briefcase
-        },
-        {
-          name: 'All Request',
-          path: '/all-background-checks',
-          icon: List
-        },
-      ]
-    },
-    {
-      name: 'Stake Holder request',
-      icon: Users,
-      children: [
-        {
-          name: 'New Request',
-          path: '/new-stake-holder-request',
-          icon: FileCheck
-        },
-        {
-          name: 'All Request',
-          path: '/all-stake-holder-request',
-          icon: List
-        },
-      ]
-    },
+    
     {
       name: 'Security Services',
       icon: Lock,
       children: [
-        {
-          name: 'Service Queue Management',
-          path: '/security-services/control-panel/queue-management',
-          icon: FileCheck
-        },
         {
           name: 'New Request',
           path: '/security-services/new-request',
@@ -307,22 +261,6 @@ export const roleBasedNavigation = {
           path: '/security-services/task',
           icon: CheckSquare
         },
-      ]
-    },
-
-    { name: 'Equipment Movement Log book', path: '/equipment-movement', icon: Wrench },
-    //{ name: 'Cleaner Profile Book', path: '/cleaner-profile-book', icon: BookOpen },
-    //{ name: 'Security Technical Issue Book', path: '/security-issue-book', icon: Bell },
-    //{ name: 'Tecnician Site Book', path: '/security-technician-fill-page', icon: Bell },
-
-    {
-      name: 'Reports',
-      icon: BarChart3,
-      children: [
-        { name: 'StakeHolder Request Report', path: '/stakeholderreport', icon: FileText },
-        { name: 'Backgroung Checks Report', path: '/backgroundcheckreport', icon: FileText },
-        { name: 'Guard Shift Report', path: '/guard-shift-report', icon: FileText },
-        {name: 'Equipment Movement Report', path: '/equipment-movement-report', icon: FileText }
       ]
     },
   ],
